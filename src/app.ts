69978848c6e34a890c1083d632964048a6b2d9de
wiktor-falek <falekwiktor@gmail.com>
2024-01-09 13:21:39 +0100

    json and urlencoded middlewares

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import exampleRouter from "./routers/exampleRouter.js";
 const app = express();
 
 // Config
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Routes
 app.use("/api", exampleRouter);
