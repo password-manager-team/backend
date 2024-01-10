b390cd125ed1026d497fa7450dc75f0c14244a0d
wiktor-falek <falekwiktor@gmail.com>
2024-01-11 00:46:42 +0100

    feat: start working on the authentication endpoints

--- src/instances.ts
+++ src/instances.ts
@@ -4,6 +4,8 @@ import ExampleService from "./services/exampleService.js"
 import config from "./config.js"
 import SessionStore from "./stores/sessionStore.js"
 import Redis from "./db/redis.js"
+import AuthController from "./controllers/authController.js"
+import AuthService from "./services/AuthService.js"
 
 const { REDIS_URL } = config
 
@@ -19,6 +21,8 @@ export const exampleModel = new ExampleModel(db)
 
 // Services
 export const exampleService = new ExampleService(exampleModel)
+export const authService = new AuthService(exampleModel, sessionStore)
 
 // Controllers
 export const exampleController = new ExampleController(exampleService)
+export const authController = new AuthController(authService)
--- src/stores/sessionStore.ts
+++ src/stores/sessionStore.ts
@@ -1,54 +1,56 @@
 import type { RedisClientType } from "redis"
 
 type SessionData = {
-  userID: string
+    userID: string
 }
 
 class SessionStore {
-  private namespace: string
-  constructor(private client: RedisClientType<any>) {
-    this.namespace = "userSessions"
-  }
+    private namespace: string
+    constructor(private client: RedisClientType<any>) {
+        this.namespace = "userSessions"
+    }
 
-  /**
-   * Retrieves session data for a given sessionID.
-   */
-  async getSession(sessionID: string) {
-    try {
-      const data = await this.client.hGet(this.namespace, sessionID)
-      if (data === undefined) {
-        return data
-      }
+    /**
+     * Retrieves session data for a given sessionID.
+     */
+    async getSession(sessionID: string) {
+        try {
+            const data = await this.client.hGet(this.namespace, sessionID)
+            if (data === undefined) {
+                return data
+            }
 
-      return JSON.parse(data) as SessionData
-    }
-    catch(e) {
-      if (e instanceof SyntaxError) {
-        console.error("Parsing error")
-      } else {
-        console.error("Unhandled Redis error:", e)
-      }
+            return JSON.parse(data) as SessionData
+        } catch (e) {
+            if (e instanceof SyntaxError) {
+                console.error("Parsing error")
+            } else {
+                console.error("Unhandled Redis error:", e)
+            }
 
-      return Promise.resolve(undefined)
+            return Promise.resolve(undefined)
+        }
     }
-  }
-  
-  /**
-   * Sets session data for a given session ID.
-   * @returns Promise that resolves with the result of the operation,
-   * 1 if a new field is created,
-   * 0 if an existing field is updated,
-   * -1 if an error occurred.
-   */
-  setSession(sessionID: string, data: SessionData) {
-    try {
-      return this.client.hSet(this.namespace, sessionID, JSON.stringify(data))
+
+    /**
+     * Sets session data for a given session ID.
+     * @returns Promise that resolves with the result of the operation,
+     * 1 if a new field is created,
+     * 0 if an existing field is updated,
+     * -1 if an error occurred.
+     */
+    setSession(sessionID: string, data: SessionData) {
+        try {
+            return this.client.hSet(
+                this.namespace,
+                sessionID,
+                JSON.stringify(data)
+            )
+        } catch (e) {
+            console.error("Unhandled Redis error:", e)
+            return Promise.resolve(-1)
+        }
     }
-    catch(e) {
-      console.error("Unhandled Redis error:", e)
-      return Promise.resolve(-1)
-    }
-  }
 }
 
 export default SessionStore
