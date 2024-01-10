39e92325574f1abfb7e92cecaf92a48bbfe8e04d
AgusDOLARD <agust_22_10@hotmail.com>
2024-01-10 19:12:46 -0300

    feat: added morgan logger

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import cookieparser from "cookie-parser"
 import exampleRouter from "./routers/exampleRouter.js"
+import morgan from "morgan"
 
 const app = express()
 
 // Config
+app.use(morgan("dev"))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cookieparser())
