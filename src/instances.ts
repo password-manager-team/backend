6041b5f5b6e3f5a2b994028ac26e5a79f04e0bb6
wiktor-falek <falekwiktor@gmail.com>
2024-01-10 14:49:42 +0100

    feat: create a session store

--- src/instances.ts
+++ src/instances.ts
@@ -1,15 +1,24 @@
-import ExampleController from "./controllers/exampleController.js";
-import ExampleModel from "./models/exampleModel.js";
-import ExampleService from "./services/exampleService.js";
+import ExampleController from "./controllers/exampleController.js"
+import ExampleModel from "./models/exampleModel.js"
+import ExampleService from "./services/exampleService.js"
+import config from "./config.js"
+import SessionStore from "./stores/sessionStore.js"
+import Redis from "./db/redis.js"
+
+const { REDIS_URL } = config
 
 // Db
-const db = null;
+const db = null
+const redisClient = await new Redis({ url: REDIS_URL }).connect()
+
+// Stores
+const sessionStore = new SessionStore(redisClient)
 
 // Models
-export const exampleModel = new ExampleModel(db);
+export const exampleModel = new ExampleModel(db)
 
 // Services
-export const exampleService = new ExampleService(exampleModel);
+export const exampleService = new ExampleService(exampleModel)
 
 // Controllers
-export const exampleController = new ExampleController(exampleService);
+export const exampleController = new ExampleController(exampleService)
