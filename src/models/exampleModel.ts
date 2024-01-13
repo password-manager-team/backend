af9acfa4e7876278955e9d5190a00282397c1027
wiktor-falek <falekwiktor@gmail.com>
2024-01-13 16:38:11 +0100

    feat: create Users table and implement adding and finding users

--- src/models/exampleModel.ts
+++ src/models/exampleModel.ts
@@ -1,18 +1,20 @@
-import type { PoolClient } from "pg";
+import type { PoolClient } from "pg"
 
 class ExampleModel {
-  constructor(private postgresClient: PoolClient) {}
+    constructor(private postgresClient: PoolClient) {}
 
-  async getExampleData() {
-    try {
-      const res = await this.postgresClient.query("SELECT * FROM TEST");
-      return res.rows;
-      
-    } catch (e) {
-      console.log(e);
-      return null;
+    async getExampleData() {
+        try {
+            const res = await this.postgresClient.query<{
+                id: number
+                name: string
+            }>("SELECT id, name FROM Test")
+            return res.rows
+        } catch (e) {
+            console.error(e)
+            return null
+        }
     }
-  }
 }
 
-export default ExampleModel;
+export default ExampleModel
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,10 +1,39 @@
+import type { PoolClient } from "pg"
+
 class UserModel {
-    constructor(private db: unknown) {}
+    constructor(private postgresClient: PoolClient) {}
 
-    createUser(email: string, passwordHash: string, passwordHint: string) {}
+    async createUser(
+        email: string,
+        passwordHash: string,
+        passwordHint: string
+    ) {
+        try {
+            const res = await this.postgresClient.query<{
+                id: number
+                name: string
+            }>(
+                "INSERT into Users (email, password_hash, password_hint) VALUES ($1, $2, $3)",
+                [email, passwordHash, passwordHint]
+            )
+            return true
+        } catch (e) {
+            console.error(e)
+            return false
+        }
+    }
 
-    findUserByEmail(email: string) {
-        return null
+    async findUserByEmail(email: string) {
+        try {
+            const res = await this.postgresClient.query<{
+                id: number
+                email: string
+            }>("SELECT id, email FROM Users WHERE email = $1", [email])
+            return res.rows[0]
+        } catch (e) {
+            console.error(e)
+            return null
+        }
     }
 }
 
