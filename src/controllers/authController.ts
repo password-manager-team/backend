503a648af20dd6ee5abb150e9f89a5daf4e1c6a5
Wiktor <falekwiktor@gmail.com>
2024-01-15 00:20:26 +0100

    Validate body in POST /auth/login (#13)
    * feat: install zod and write first schema
    
    * fix: pass the req object into validation
    
    * feat: schema validation in AuthController.login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,18 +1,26 @@
 import type { Request, Response } from "express"
 import type AuthService from "../services/AuthService.js"
+import { z } from "zod"
 
 class AuthController {
     constructor(private authService: AuthService) {}
 
-    async register(req: Request, res: Response) {
-        // TODO: req.body schema validation
-        // email: string, passwordHash: string, passwordHint: string
+    async postRegister(req: Request, res: Response) {
+        const schema = z.object({
+            body: z.object({
+                email: z.string().email().min(6).max(254),
+                passwordHash: z.string().max(512),
+                passwordHint: z.string().min(1).max(32),
+            }),
+        })
 
-        // replace code below with
-        // const { email, passwordHash, passwordHint } = validatedData
-        const email = "TODO"
-        const passwordHash = "TODO"
-        const passwordHint = "TODO"
+        const validationResult = schema.safeParse(req)
+
+        if (!validationResult.success) {
+            return res.status(422).json({ error: "Invalid data" })
+        }
+
+        const { email, passwordHash, passwordHint } = validationResult.data.body
 
         const user = await this.authService.register(
             email,
@@ -29,14 +37,21 @@ class AuthController {
         return res.status(200).json({ message: "Created an account" })
     }
 
-    async login(req: Request, res: Response) {
-        // TODO: req.body schema validation
-        // email: string, passwordHash: string
+    async postLogin(req: Request, res: Response) {
+        const schema = z.object({
+            body: z.object({
+                email: z.string().email().min(6).max(254),
+                passwordHash: z.string().max(512),
+            }),
+        })
 
-        // replace code below with
-        // const { email, passwordHash } = validatedData
-        const email = "TODO"
-        const passwordHash = "TODO"
+        const validationResult = schema.safeParse(req)
+
+        if (!validationResult.success) {
+            return res.status(422).json({ error: "Invalid data" })
+        }
+
+        const { email, passwordHash } = validationResult.data.body
 
         const sessionID = await this.authService.login(email, passwordHash)
 
--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -3,7 +3,7 @@ import { authController } from "../instances.js"
 
 const router = Router()
 
-router.post("/register", authController.register.bind(authController))
-router.post("/login", authController.login.bind(authController))
+router.post("/register", authController.postRegister.bind(authController))
+router.post("/login", authController.postLogin.bind(authController))
 
 export default router
