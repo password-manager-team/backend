1bb596812290cd6500522c1bddfea31ee13612ae
wiktor-falek <falekwiktor@gmail.com>
2024-01-15 01:05:33 +0100

    feat: send a sessionID cookie

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express"
 import type AuthService from "../services/AuthService.js"
 import { z } from "zod"
+import config from "../config.js"
 
 class AuthController {
     constructor(private authService: AuthService) {}
@@ -51,12 +52,10 @@ class AuthController {
         const validationResult = schema.safeParse(req)
 
         if (!validationResult.success) {
-            return res
-                .status(422)
-                .json({
-                    error: "Invalid data",
-                    stacktrace: validationResult.error.issues,
-                })
+            return res.status(422).json({
+                error: "Invalid data",
+                stacktrace: validationResult.error.issues,
+            })
         }
 
         const { email, passwordHash } = validationResult.data.body
@@ -67,7 +66,12 @@ class AuthController {
             return res.status(401).json({ error: "Invalid email or password" })
         }
 
-        // TODO: attach a sessionID cookie
+        res.cookie("sessionID", sessionID, {
+            httpOnly: true,
+            sameSite: "none",
+            maxAge: 1000 * 60 * 60 * 24 * 365,
+            secure: config.NODE_ENV !== "development",
+        })
 
         return res.status(200).json({ message: "Logged in succesfully" })
     }
