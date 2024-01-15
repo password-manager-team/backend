d0080b5211503044318d5947cacc8b036e832623
wiktor-falek <falekwiktor@gmail.com>
2024-01-15 01:00:48 +0100

    feat: authenticate the user and generate a sessionID

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from "crypto"
 import type UserModel from "../models/userModel.js"
 import type SessionStore from "../stores/sessionStore.js"
 
@@ -12,12 +13,29 @@ class AuthService {
     }
 
     async login(email: string, passwordHash: string) {
-        // TODO: check if credentials are correct
+        const user = await this.userModel.findUserByEmail(email)
 
-        // TODO: generate a sessionID and persist in sessionStore
+        if (user === null) {
+            return null
+        }
 
-        // TODO: return sessionID
-        return null
+        const passwordIsCorrect = passwordHash === user.password_hash
+
+        if (!passwordIsCorrect) {
+            return null
+        }
+
+        const sessionID = randomUUID() as string
+
+        const writeSuccess = await this.sessionStore.setSession(sessionID, {
+            userID: user.id.toString(),
+        })
+
+        if (!writeSuccess) {
+            return null
+        }
+
+        return sessionID
     }
 }
 
