84dd23a37bbf9706b579ab89dc61061b45bb3627
ps251 <>
2024-01-22 22:50:04 +0100

    Feat: model vault in postgres

--- src/db/postgres.ts
+++ src/db/postgres.ts
@@ -32,8 +32,56 @@ export class Postgres {
                     password_hint VARCHAR(32) NOT NULL
                 );
             `),
+            client.query(`
+                CREATE TABLE IF NOT EXISTS Folder(
+                    id SERIAL NOT NULL PRIMARY KEY,
+                    folder_name VARCHAR(31) NOT NULL,
+                    position INTEGER,
+                    user_id INTEGER REFERENCES Users(id) ON DELETE CASCADE
+                );
+                CREATE TABLE IF NOT EXISTS Vault(
+                    id SERIAL NOT NULL PRIMARY KEY,
+                    domain VARCHAR(255) NOT NULL, 
+                    username_email VARCHAR(255) NOT NULL, 
+                    password VARCHAR(255) NOT NULL,
+                    folder_id INTEGER REFERENCES Folder(id) ON DELETE SET NULL, 
+                    user_id INTEGER REFERENCES Users(id) ON DELETE CASCADE, 
+                    UNIQUE(user_id, domain, username_email)
+                );
+                CREATE INDEX IF NOT EXISTS idx_vault_user_id_folder_id ON Vault(user_id, folder_id) INCLUDE (domain, username_email, password);
+                CREATE INDEX IF NOT EXISTS idx_folder_user_id ON Folder(user_id) INCLUDE (folder_name);
+            `),
+            // test data
+            client.query(`
+                DELETE FROM Users;
+                DELETE FROM Vault;
+                DELETE FROM Folder;
+            `),
+            client.query(`
+                DELETE FROM Users;
+                DELETE FROM Vault;
+                DELETE FROM Folder;
+                INSERT INTO Users (id, email, password_hash, password_hint)
+                VALUES 
+                    (1, 'user1@example.com', 'hash1', 'hint1'),
+                    (2, 'user2@example.com', 'hash2', 'hint2'),
+                    (3, 'user3@example.com', 'hash3', 'hint3');
+            `),
+            client.query(`
+                INSERT INTO Folder (id, folder_name, position, user_id)
+                VALUES 
+                    (1, 'Work', 1, 1),
+                    (2, 'Personal', 2, 1),
+                    (3, 'Travel', 1, 2);
+            `),
+            client.query(`
+                INSERT INTO Vault (id, domain, username_email, password, folder_id, user_id)
+                VALUES 
+                    (1, 'example.com', 'user1@example.com', 'password1', 1, 1),
+                    (2, 'work.com', 'contact@work.com', 'password2', 1, 1),
+                    (3, 'travel.com', 'booking@travel.com', 'password3', 3, 2);
+            `),
         ])
-
         return client
     }
 
