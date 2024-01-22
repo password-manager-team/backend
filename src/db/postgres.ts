ca1009041177b8e38f422e2b864ee0856adefc82
ps251 <>
2024-01-23 00:20:52 +0100

    fix: change table schema

--- src/db/postgres.ts
+++ src/db/postgres.ts
@@ -33,53 +33,48 @@ export class Postgres {
                 );
             `),
             client.query(`
-                CREATE TABLE IF NOT EXISTS Folder(
-                    id SERIAL NOT NULL PRIMARY KEY,
-                    folder_name VARCHAR(31) NOT NULL,
-                    position INTEGER,
-                    user_id INTEGER REFERENCES Users(id) ON DELETE CASCADE
+                CREATE TABLE IF NOT EXISTS Vaults (
+                  id SERIAL PRIMARY KEY,
+                  userID INT REFERENCES Users(id) ON DELETE CASCADE
                 );
-                CREATE TABLE IF NOT EXISTS Vault(
-                    id SERIAL NOT NULL PRIMARY KEY,
-                    domain VARCHAR(255) NOT NULL, 
-                    username_email VARCHAR(255) NOT NULL, 
-                    password VARCHAR(255) NOT NULL,
-                    folder_id INTEGER REFERENCES Folder(id) ON DELETE SET NULL, 
-                    user_id INTEGER REFERENCES Users(id) ON DELETE CASCADE, 
-                    UNIQUE(user_id, domain, username_email)
+                CREATE TABLE IF NOT EXISTS Folders (
+                  id SERIAL PRIMARY KEY,
+                  vaultID INT REFERENCES Vaults(id) ON DELETE CASCADE,
+                  name VARCHAR(255) NOT NULL
                 );
-                CREATE INDEX IF NOT EXISTS idx_vault_user_id_folder_id ON Vault(user_id, folder_id) INCLUDE (domain, username_email, password);
-                CREATE INDEX IF NOT EXISTS idx_folder_user_id ON Folder(user_id) INCLUDE (folder_name);
+                CREATE TABLE IF NOT EXISTS Items (
+                  id SERIAL PRIMARY KEY,
+                  folderID INT REFERENCES Folders(id) ON DELETE CASCADE,
+                  name VARCHAR(255) NULL,
+                  domain VARCHAR(255) NOT NULL,
+                  login VARCHAR(255) NOT NULL,
+                  password VARCHAR(255) NOT NULL,
+                  UNIQUE(folderID, domain, login)
+                );
+                CREATE INDEX IF NOT EXISTS idx_vaults_userID ON Vaults(userID);
+                CREATE INDEX IF NOT EXISTS idx_folders_vaultID ON Folders(vaultID);
+                CREATE INDEX IF NOT EXISTS dx_items_folderID ON Items(folderID);
             `),
-            // test data
+
             client.query(`
                 DELETE FROM Users;
-                DELETE FROM Vault;
-                DELETE FROM Folder;
-            `),
-            client.query(`
-                DELETE FROM Users;
-                DELETE FROM Vault;
-                DELETE FROM Folder;
-                INSERT INTO Users (id, email, password_hash, password_hint)
-                VALUES 
-                    (1, 'user1@example.com', 'hash1', 'hint1'),
-                    (2, 'user2@example.com', 'hash2', 'hint2'),
-                    (3, 'user3@example.com', 'hash3', 'hint3');
-            `),
-            client.query(`
-                INSERT INTO Folder (id, folder_name, position, user_id)
-                VALUES 
-                    (1, 'Work', 1, 1),
-                    (2, 'Personal', 2, 1),
-                    (3, 'Travel', 1, 2);
-            `),
-            client.query(`
-                INSERT INTO Vault (id, domain, username_email, password, folder_id, user_id)
-                VALUES 
-                    (1, 'example.com', 'user1@example.com', 'password1', 1, 1),
-                    (2, 'work.com', 'contact@work.com', 'password2', 1, 1),
-                    (3, 'travel.com', 'booking@travel.com', 'password3', 3, 2);
+                DELETE FROM Vaults;
+                DELETE FROM Folders;
+                DELETE FROM Items;
+                INSERT INTO Users (id, email, password_hash, password_hint) VALUES
+                (1, 'user1@example.com', 'hashedpassword1', 'My first pet’s name'),
+                (2, 'user2@example.com', 'hashedpassword2', 'My favorite movie');
+                INSERT INTO Vaults (id, userID) VALUES
+                (1, 1),
+                (2, 2);
+                INSERT INTO Folders (id, vaultID, name) VALUES
+                (1, 1, 'Personal'),
+                (2, 1, 'Work'),
+                (3, 2, 'Family');
+                INSERT INTO Items (id, folderID, name, domain, login, password) VALUES
+                (1, 1, 'Email Account', 'email.com', 'user1', 'password123'),
+                (2, 2, 'Work Portal', 'work.com', 'user1work', 'workpass456'),
+                (3, 3, 'Social Media', 'social.com', 'familyuser', 'familypass789');
             `),
         ])
         return client
